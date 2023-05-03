0b692eea4337d3cc0ca4b6b64753bf9ab9cd51ab
Egoi <egoiastiz@gmail.com>
2023-05-03 23:54:33 +0200

    first session

---
+++ login.php
@@ -0,0 +1,12 @@
+<?php //include("functions.php") ?>
+<?php include("partes/head.php") ?>
+
+<pre>
+<?php //var_dump($_SERVER) ?>
+</pre>
+
+<?php $conexion = conectar(1);
+var_dump($conexion) ?>
+
+
+<?php include("partes/footer.php") ?>
